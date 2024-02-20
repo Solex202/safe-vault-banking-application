bb5f0608b80773e1b196eac7498f138f04afba0e
Solex202 <onwukalotachukwu210@gmail.com>
2024-02-20 15:54:30 +0100

    implementing application level security

---
+++ src/main/java/com/lota/SafeVaultBankingApplication/utils/AppUtil.java
@@ -0,0 +1,7 @@
+package com.lota.SafeVaultBankingApplication.utils;
+
+public class AppUtil {
+
+    public static final String EMAIL= "email";
+    public static final String APPLICATION_JSON= "application/json";
+}
