28ce8d866c028cefcb5db4bd713587411d43695c
Solex202 <onwukalotachukwu210@gmail.com>
2024-02-20 17:07:50 +0100

    implementing application level security

--- src/main/java/com/lota/SafeVaultBankingApplication/models/SafeVaultUserProfile.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/models/SafeVaultUserProfile.java
@@ -1,17 +1,32 @@
 package com.lota.SafeVaultBankingApplication.models;
 
 import com.lota.SafeVaultBankingApplication.models.NextOfKin;
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+@Data
+@AllArgsConstructor@NoArgsConstructor@Builder
 public class SafeVaultUserProfile {
 
+    @Id
+    private String id;
+    //personal information
     private String dob;
-    private String age;
-    private String motherMaidenName;
-    private String address;
-    private String occupation;
-    private String phoneNumber;
-    private NextOfKin nextOfKin;
+    private int age;
     private String gender;
-    private String stateOfOrigin;
+    private String motherMaidenName;
+    private String image;
+    //contact information
+    private String phoneNumber;
+    private String address;
     private String stateOfResidence;
+    private String stateOfOrigin;
+    //employment information
+    private String occupation;
+    private String employerName;
+
+    private NextOfKin nextOfKin;
+    private String bvn;
 }
