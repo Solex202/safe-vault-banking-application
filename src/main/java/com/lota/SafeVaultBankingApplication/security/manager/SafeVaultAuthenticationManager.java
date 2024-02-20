891150249e8c50021da6766e42e967392f5534ba
Solex202 <onwukalotachukwu210@gmail.com>
2024-02-20 13:04:54 +0100

    implementing application level security

---
+++ src/main/java/com/lota/SafeVaultBankingApplication/security/manager/SafeVaultAuthenticationManager.java
@@ -0,0 +1,26 @@
+package com.lota.SafeVaultBankingApplication.security.manager;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderNotFoundException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+import static com.lota.SafeVaultBankingApplication.exceptions.ExceptionMessages.UNSUPPORTED_AUTHENTICATION_TYPE;
+
+@AllArgsConstructor
+
+public class SafeVaultAuthenticationManager implements AuthenticationManager {
+
+    private final AuthenticationProvider authenticationProvider;
+
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        boolean isAuthenticationSupported = authenticationProvider.supports(authentication.getClass());
+        if (isAuthenticationSupported) authenticationProvider.authenticate(authentication);
+
+        throw new ProviderNotFoundException(UNSUPPORTED_AUTHENTICATION_TYPE.getMessage());
+    }
+}
