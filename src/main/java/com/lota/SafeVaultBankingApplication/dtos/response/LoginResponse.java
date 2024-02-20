5f87ac5c9092d51a75f8657bbbd2c9847f3bc00b
Solex202 <onwukalotachukwu210@gmail.com>
2024-02-20 12:15:17 +0100

    implementing application level security

--- src/main/java/com/lota/SafeVaultBankingApplication/dtos/response/LoginResponse.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/dtos/response/LoginResponse.java
@@ -3,7 +3,5 @@ package com.lota.SafeVaultBankingApplication.dtos.response;
 public class LoginResponse {
 
     private String token;
-    private String token2;
-    private String token3;
-    private String toke4;
+
 }
