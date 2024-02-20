c016b92e3375db4d9346f14de27db3f96584e569
Solex202 <onwukalotachukwu210@gmail.com>
2024-02-20 10:48:06 +0100

    first commit

---
+++ src/main/java/com/lota/SafeVaultBankingApplication/SafeVaultBankingApplication.java
@@ -0,0 +1,13 @@
+package com.lota.SafeVaultBankingApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeVaultBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeVaultBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lota/SafeVaultBankingApplication/SafeVaultBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lota.SafeVaultBankingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeVaultBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
