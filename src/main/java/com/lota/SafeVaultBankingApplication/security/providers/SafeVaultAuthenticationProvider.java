9cbf2e676266917a8183d44c744ac1239db355ea
Solex202 <onwukalotachukwu210@gmail.com>
2024-02-20 13:04:33 +0100

    implementing application level security

---
+++ src/main/java/com/lota/SafeVaultBankingApplication/security/providers/SafeVaultAuthenticationProvider.java
@@ -0,0 +1,19 @@
+package com.lota.SafeVaultBankingApplication.security.providers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+@AllArgsConstructor
+public class SafeVaultAuthenticationProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
+}
