1324085ca43e4f4182795c51af78a32d46945665
Solex202 <onwukalotachukwu210@gmail.com>
2024-02-20 16:40:52 +0100

    implementing application level security

---
+++ src/main/java/com/lota/SafeVaultBankingApplication/models/SafeVaultUserProfile.java
@@ -0,0 +1,17 @@
+package com.lota.SafeVaultBankingApplication.models;
+
+import com.lota.SafeVaultBankingApplication.models.NextOfKin;
+
+public class SafeVaultUserProfile {
+
+    private String dob;
+    private String age;
+    private String motherMaidenName;
+    private String address;
+    private String occupation;
+    private String phoneNumber;
+    private NextOfKin nextOfKin;
+    private String gender;
+    private String stateOfOrigin;
+    private String stateOfResidence;
+}
