97437b3827b18197a0488f890999663db83ea741
Solex202 <onwukalotachukwu210@gmail.com>
2024-02-20 13:35:42 +0100

    implementing application level security

---
+++ src/main/java/com/lota/SafeVaultBankingApplication/security/models/Principal.java
@@ -0,0 +1,57 @@
+package com.lota.SafeVaultBankingApplication.security.models;
+
+import com.lota.SafeVaultBankingApplication.models.SafeVaultUser;
+import com.lota.SafeVaultBankingApplication.models.enums.Authority;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toSet;
+
+@AllArgsConstructor
+public class Principal implements UserDetails {
+
+    private final SafeVaultUser user;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Set<Authority> authorities = user.getAuthorities();
+        return authorities.stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.name()))
+                .collect(toSet());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
