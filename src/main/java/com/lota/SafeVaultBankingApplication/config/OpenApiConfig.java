de556430bf777eef38c97e98bc7f189bc837037c
Solex202 <onwukalotachukwu210@gmail.com>
2024-02-21 09:38:19 +0100

    Configuring swagger UI

--- src/main/java/com/lota/SafeVaultBankingApplication/config/OpenApiConfig.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/config/OpenApiConfig.java
@@ -1,5 +1,28 @@
 package com.lota.SafeVaultBankingApplication.config;
 
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.Servers;
+
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "Safe Vault Banking Application Documentation",
+                description = "Documentation for Safe Vault Banking Application",
+                version = "1.0"
+        ),
+        servers = {
+                @Server(
+                     description = "local",
+                     url = "http://localhost:9639"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
 public class OpenApiConfig {
 }
