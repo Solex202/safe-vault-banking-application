969b3dcbc0dbdf9a69039f72ff1e12b5cc97f071
Solex202 <onwukalotachukwu210@gmail.com>
2024-02-21 09:40:18 +0100

    Configuring swagger UI

--- src/main/java/com/lota/SafeVaultBankingApplication/config/OpenApiConfig.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/config/OpenApiConfig.java
@@ -2,6 +2,8 @@ package com.lota.SafeVaultBankingApplication.config;
 
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.annotations.servers.Servers;
@@ -23,6 +25,15 @@ import io.swagger.v3.oas.annotations.servers.Servers;
                         name = "bearerAuth"
                 )
         }
+)
+@io.swagger.v3.oas.annotations.security.SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+
 )
 public class OpenApiConfig {
 }
