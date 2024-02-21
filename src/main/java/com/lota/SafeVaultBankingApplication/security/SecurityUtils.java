70c5af05b6986f1e7d7beb0b4f52e637de57d504
Solex202 <onwukalotachukwu210@gmail.com>
2024-02-21 15:25:59 +0100

    implementing application level security

--- src/main/java/com/lota/SafeVaultBankingApplication/security/SecurityUtils.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/security/SecurityUtils.java
@@ -1,4 +1,12 @@
 package com.lota.SafeVaultBankingApplication.security;
 
+import java.util.Set;
+
 public class SecurityUtils {
+
+    public static Set<String> getAuthenticationWhiteList(){
+        return Set.of(
+
+        );
+    }
 }
