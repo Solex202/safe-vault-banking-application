c7a2ecf421be6941cb3452b1e6d0847b60c04a9a
Solex202 <onwukalotachukwu210@gmail.com>
2024-02-22 22:43:18 +0100

    implementing application level security

--- src/main/java/com/lota/SafeVaultBankingApplication/config/AppConfig.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/config/AppConfig.java
@@ -1,4 +1,12 @@
 package com.lota.SafeVaultBankingApplication.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+
 public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 }
