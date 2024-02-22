219dfa7df44344b0a20ed4adff9caad3bc51ee64
Solex202 <onwukalotachukwu210@gmail.com>
2024-02-22 22:39:10 +0100

    implementing application level security

--- src/main/java/com/lota/SafeVaultBankingApplication/security/filters/SafeVaultAuthorizationFilter.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/security/filters/SafeVaultAuthorizationFilter.java
@@ -1,7 +1,18 @@
 package com.lota.SafeVaultBankingApplication.security.filters;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @AllArgsConstructor
-public class SafeVaultAuthorizationFilter {
+public class SafeVaultAuthorizationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
 }
