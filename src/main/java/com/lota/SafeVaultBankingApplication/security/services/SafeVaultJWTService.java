57917e10949c9c01d0df768accfae580ab143480
Solex202 <onwukalotachukwu210@gmail.com>
2024-03-26 16:37:00 +0100

    jwt implementation

--- src/main/java/com/lota/SafeVaultBankingApplication/security/services/SafeVaultJWTService.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/security/services/SafeVaultJWTService.java
@@ -1,5 +1,9 @@
 package com.lota.SafeVaultBankingApplication.security.services;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 public interface SafeVaultJWTService {
     String generateTokenFor(String email);
+
+    UserDetails extraUserDetailsFromToken(String token);
 }
