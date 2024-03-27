410b1a35d69b690db6d66a7da3aab05cceb489a6
Solex202 <onwukalotachukwu210@gmail.com>
2024-03-27 16:23:01 +0100

    Sun's gon shine on everything you do

--- src/main/java/com/lota/SafeVaultBankingApplication/security/SecurityUtils.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/security/SecurityUtils.java
@@ -6,7 +6,12 @@ public class SecurityUtils {
 
     public static Set<String> getAuthenticationWhiteList(){
         return Set.of(
-
+                "/auth/login",
+                "/users",
+                "/v3/api-docs/**",
+                "/swagger-ui/**",
+                "/swagger-ui.html",
+                "/v3/api-docs.yaml"
         );
     }
 }
