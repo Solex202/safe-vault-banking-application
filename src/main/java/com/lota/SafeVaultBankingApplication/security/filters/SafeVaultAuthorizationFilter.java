baa6cab28f01de78dcc701380764f5d0a7d3fc4c
Solex202 <onwukalotachukwu210@gmail.com>
2024-03-27 16:23:07 +0100

    Sun's gon shine on everything you do

--- src/main/java/com/lota/SafeVaultBankingApplication/security/filters/SafeVaultAuthorizationFilter.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/security/filters/SafeVaultAuthorizationFilter.java
@@ -1,5 +1,6 @@
 package com.lota.SafeVaultBankingApplication.security.filters;
 
+import com.lota.SafeVaultBankingApplication.security.services.SafeVaultJWTService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,6 +12,8 @@ import java.io.IOException;
 
 @AllArgsConstructor
 public class SafeVaultAuthorizationFilter extends OncePerRequestFilter {
+
+    private final SafeVaultJWTService safeVaultJWTService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
