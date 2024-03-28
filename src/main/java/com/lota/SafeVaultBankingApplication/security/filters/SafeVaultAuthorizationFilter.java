9939943e9702c591985d55ddf97d02d6e2af389a
Solex202 <onwukalotachukwu210@gmail.com>
2024-03-28 16:47:40 +0100

    Sun's gon shine on everything you do

--- src/main/java/com/lota/SafeVaultBankingApplication/security/filters/SafeVaultAuthorizationFilter.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/security/filters/SafeVaultAuthorizationFilter.java
@@ -7,6 +7,9 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -40,6 +43,10 @@ public class SafeVaultAuthorizationFilter extends OncePerRequestFilter {
     }
 
     private void authorize(UserDetails userDetails) {
+
+        Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userDetails.getUsername(), null, authorities);
+        SecurityContextHolder.getContext().setAuthentication(token);
     }
 
     private String extractTokenFrom(String authorizationHeader) {
