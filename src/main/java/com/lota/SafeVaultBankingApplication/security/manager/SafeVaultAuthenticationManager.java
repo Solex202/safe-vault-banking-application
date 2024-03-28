088e7baf450274ffaf5c346090d4e657d212e80a
Solex202 <onwukalotachukwu210@gmail.com>
2024-03-28 17:24:41 +0100

    Sun's gon shine on everything you do

--- src/main/java/com/lota/SafeVaultBankingApplication/security/manager/SafeVaultAuthenticationManager.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/security/manager/SafeVaultAuthenticationManager.java
@@ -1,16 +1,18 @@
 package com.lota.SafeVaultBankingApplication.security.manager;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
 
 import static com.lota.SafeVaultBankingApplication.exceptions.ExceptionMessages.UNSUPPORTED_AUTHENTICATION_TYPE;
 
 @AllArgsConstructor
-
+@Component
 public class SafeVaultAuthenticationManager implements AuthenticationManager {
 
     private final AuthenticationProvider authenticationProvider;
