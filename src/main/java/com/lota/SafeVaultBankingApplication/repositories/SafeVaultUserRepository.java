1353d1c03a038f5f3df552f4be0c7d6b54e608c1
Solex202 <onwukalotachukwu210@gmail.com>
2024-04-01 11:49:41 +0100

    Life goes on. . .

--- src/main/java/com/lota/SafeVaultBankingApplication/repositories/SafeVaultUserRepository.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/repositories/SafeVaultUserRepository.java
@@ -6,8 +6,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface SafeVaultUserRepository extends MongoRepository<SafeVaultUser, String> {
-
-
-
     Optional<SafeVaultUser> findByEmail(String email);
 }
