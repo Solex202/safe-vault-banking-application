c32f1b3977fff8d058b42bd336aa9b63e94690f8
Solex202 <onwukalotachukwu210@gmail.com>
2024-04-02 10:18:24 +0100

    In God we trust, every other thing we test

---
+++ src/main/java/com/lota/SafeVaultBankingApplication/exceptions/AppException.java
@@ -0,0 +1,8 @@
+package com.lota.SafeVaultBankingApplication.exceptions;
+
+public class AppException extends RuntimeException{
+
+   public AppException(String message){
+        super(message);
+   }
+}
