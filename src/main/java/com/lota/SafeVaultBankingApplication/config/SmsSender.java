0342b87e6c57f17d298289f38fb6e13d6ae4b569
Solex202 <onwukalotachukwu210@gmail.com>
2024-04-09 10:07:12 +0100

    In God we trust, every other thing we test

--- src/main/java/com/lota/SafeVaultBankingApplication/config/SmsSender.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/config/SmsSender.java
@@ -3,7 +3,6 @@ package com.lota.SafeVaultBankingApplication.config;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
-import org.modelmapper.internal.bytebuddy.utility.RandomString;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.annotation.Value;
 
