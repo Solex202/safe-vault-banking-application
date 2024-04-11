4c2c019a3e1f655f58200822654e02316adce69e
Solex202 <onwukalotachukwu210@gmail.com>
2024-04-11 13:16:57 +0100

    banner.txt creation
    user registration flow --saving phone number and otp

--- src/main/java/com/lota/SafeVaultBankingApplication/services/SafeVaultUserService.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/services/SafeVaultUserService.java
@@ -1,13 +1,12 @@
 package com.lota.SafeVaultBankingApplication.services;
 
-import com.lota.SafeVaultBankingApplication.dtos.request.RegisterRequest;
 import com.lota.SafeVaultBankingApplication.models.SafeVaultUser;
 
 public interface SafeVaultUserService {
 
     SafeVaultUser getUserBy(String searchParam);
 
-    void registerUser(String phoneNumber);
+    void processUserPhoneNumber(String phoneNumber);
 
 
 }
