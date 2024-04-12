b532b6b5a1a9d06ba36fe510371e6762fd92d5c1
Solex202 <onwukalotachukwu210@gmail.com>
2024-04-12 12:00:29 +0100

    banner.txt creation
    user registration flow -- testing otp validation

--- src/main/java/com/lota/SafeVaultBankingApplication/exceptions/SuccessMessage.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/exceptions/SuccessMessage.java
@@ -5,7 +5,10 @@ import lombok.Getter;
 @Getter
 public enum SuccessMessage {
 
-    OTP_VERIFIED_SUCCESSFULLY("OTP VERIFIED SUCCESSFULLY");
+    OTP_VERIFIED_SUCCESSFULLY("OTP VERIFIED SUCCESSFULLY"),
+
+    OTP_RESENT_SUCCESSFULLY("A new otp has been sent to your inbox")
+    ;
 
     private final  String message;
     SuccessMessage(String message){
