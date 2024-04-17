5bd7ea1da5b62bac75451617fed2d51d3951f438
Solex202 <onwukalotachukwu210@gmail.com>
2024-04-17 09:54:28 +0100

    user registration flow -- updating user profile

---
+++ src/main/java/com/lota/SafeVaultBankingApplication/services/SafeVaultUserProfileServiceImpl.java
@@ -0,0 +1,12 @@
+package com.lota.SafeVaultBankingApplication.services;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class SafeVaultUserProfileServiceImpl implements SafeVaultUserProfileService{
+}
