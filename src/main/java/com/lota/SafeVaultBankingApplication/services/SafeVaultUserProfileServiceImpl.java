d70997ab89437c3a93d7c6aab749c738969da611
Solex202 <onwukalotachukwu210@gmail.com>
2024-04-17 11:55:09 +0100

    user registration flow -- updating user profile

--- src/main/java/com/lota/SafeVaultBankingApplication/services/SafeVaultUserProfileServiceImpl.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/services/SafeVaultUserProfileServiceImpl.java
@@ -3,11 +3,15 @@ package com.lota.SafeVaultBankingApplication.services;
 
 import com.lota.SafeVaultBankingApplication.dtos.request.CreateProfileRequest;
 import com.lota.SafeVaultBankingApplication.dtos.response.CreateProfileResponse;
+import com.lota.SafeVaultBankingApplication.models.SafeVaultUserProfile;
 import com.lota.SafeVaultBankingApplication.repositories.SafeVaultUserProfileRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Period;
 
 @Service
 @RequiredArgsConstructor
@@ -17,7 +21,21 @@ public class SafeVaultUserProfileServiceImpl implements SafeVaultUserProfileServ
     private final SafeVaultUserProfileRepository safeVaultUserProfileRepository;
     @Override
     public CreateProfileResponse createUserProfile(CreateProfileRequest request) {
-        CreateProfileResponse response = mapper.map(request, CreateProfileResponse.class);
+
+        SafeVaultUserProfile safeVaultUserProfile = mapper.map(request, SafeVaultUserProfile.class);
+        safeVaultUserProfile.setAge(calculateAge(request.getDateOfBirth()));
         return null;
+    }
+
+
+    public int calculateAge(LocalDate dateOfBirth) {
+        int age = 0;
+        if (dateOfBirth != null) {
+            LocalDate currentDate = LocalDate.now();
+            Period period = Period.between(dateOfBirth, currentDate);
+            age = period.getYears();
+        }
+
+        return age;
     }
 }
