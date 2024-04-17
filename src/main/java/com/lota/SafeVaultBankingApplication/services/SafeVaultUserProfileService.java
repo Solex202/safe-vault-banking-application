f409aae1eca2f19e1796a8d8aa650f06ed37513a
Solex202 <onwukalotachukwu210@gmail.com>
2024-04-17 10:15:44 +0100

    user registration flow -- updating user profile

--- src/main/java/com/lota/SafeVaultBankingApplication/services/SafeVaultUserProfileService.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/services/SafeVaultUserProfileService.java
@@ -1,4 +1,9 @@
 package com.lota.SafeVaultBankingApplication.services;
 
+import com.lota.SafeVaultBankingApplication.dtos.request.CreateProfileRequest;
+import com.lota.SafeVaultBankingApplication.dtos.response.CreateProfileResponse;
+
 public interface SafeVaultUserProfileService {
+
+    CreateProfileResponse createUserProfile(CreateProfileRequest request);
 }
