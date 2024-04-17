253e6e6a496d0ae8a6fa860cd6303ca75c478344
Solex202 <onwukalotachukwu210@gmail.com>
2024-04-17 13:25:37 +0100

    user registration flow -- updating user profile

--- src/main/java/com/lota/SafeVaultBankingApplication/dtos/response/CreateProfileResponse.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/dtos/response/CreateProfileResponse.java
@@ -1,10 +1,31 @@
 package com.lota.SafeVaultBankingApplication.dtos.response;
 
+import com.lota.SafeVaultBankingApplication.models.NextOfKin;
 import lombok.*;
+
+import java.time.LocalDate;
 
 @Data
 @Builder
 @AllArgsConstructor
 @RequiredArgsConstructor
 public class CreateProfileResponse {
+    private LocalDate dateOfBirth;
+    private int age;
+    private String gender;
+    private String motherMaidenName;
+
+    //contact information
+    private String phoneNumber;
+    private String address;
+    private String stateOfResidence;
+    private String stateOfOrigin;
+    //employment information
+    private String occupation;
+    private String employerName;
+    private String employerContact;
+
+    private NextOfKin nextOfKin;
+    private String bvn;
+
 }
