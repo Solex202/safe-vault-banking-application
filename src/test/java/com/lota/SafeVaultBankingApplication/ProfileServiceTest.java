e1375ab7064ced4dd3b049d0cd62bc170fea2375
Solex202 <onwukalotachukwu210@gmail.com>
2024-04-17 13:25:46 +0100

    user registration flow -- updating user profile

---
+++ src/test/java/com/lota/SafeVaultBankingApplication/ProfileServiceTest.java
@@ -0,0 +1,37 @@
+package com.lota.SafeVaultBankingApplication;
+
+import com.lota.SafeVaultBankingApplication.dtos.request.CreateProfileRequest;
+import com.lota.SafeVaultBankingApplication.dtos.response.CreateProfileResponse;
+import com.lota.SafeVaultBankingApplication.services.SafeVaultUserProfileService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class ProfileServiceTest {
+
+    @Autowired
+    private SafeVaultUserProfileService profileService;
+
+
+    @Test
+    void createProfile(){
+
+        CreateProfileRequest request = CreateProfileRequest.builder()
+                .dateOfBirth(LocalDate.of(1994, 3,21))
+                .gender("MALE")
+                .stateOfOrigin("Original state")
+                .bvn("322213232121")
+                .build();
+
+        CreateProfileResponse response = profileService.createUserProfile("661908db349dd6078964982c", request);
+        assertThat(response.getAge(), is(30));
+
+
+    }
+}
