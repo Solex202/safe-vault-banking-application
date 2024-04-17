5b3e6e9ff213698dfb28aebc0d757442939ef533
Solex202 <onwukalotachukwu210@gmail.com>
2024-04-17 10:16:53 +0100

    user registration flow -- updating user profile

--- src/main/java/com/lota/SafeVaultBankingApplication/config/AppConfig.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/config/AppConfig.java
@@ -2,7 +2,9 @@ package com.lota.SafeVaultBankingApplication.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     @Bean
