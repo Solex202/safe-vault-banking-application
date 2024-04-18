58ef9885da6f424de7257bb854d5429b70ea524d
Solex202 <onwukalotachukwu210@gmail.com>
2024-04-18 17:08:12 +0100

    updating user profile

---
+++ src/main/java/com/lota/SafeVaultBankingApplication/models/ApplicationMapper.java
@@ -0,0 +1,9 @@
+package com.lota.SafeVaultBankingApplication.models;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface ApplicationMapper {
+}
