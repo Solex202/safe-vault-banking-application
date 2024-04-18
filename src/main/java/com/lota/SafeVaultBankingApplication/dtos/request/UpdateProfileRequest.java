e7797cf0c40a5353ecabefb952407dfab3c5ab96
Solex202 <onwukalotachukwu210@gmail.com>
2024-04-18 17:07:55 +0100

    user registration flow -- updating user profile

---
+++ src/main/java/com/lota/SafeVaultBankingApplication/dtos/request/UpdateProfileRequest.java
@@ -0,0 +1,13 @@
+package com.lota.SafeVaultBankingApplication.dtos.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
+public class UpdateProfileRequest {
+}
