5b0ab746418710d0492c947c91c3bea4f5fd7e4d
Solex202 <onwukalotachukwu210@gmail.com>
2024-04-19 23:15:16 +0100

    updating user profile

--- src/main/java/com/lota/SafeVaultBankingApplication/dtos/request/UpdateProfileRequest.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/dtos/request/UpdateProfileRequest.java
@@ -1,13 +1,32 @@
 package com.lota.SafeVaultBankingApplication.dtos.request;
 
+import com.lota.SafeVaultBankingApplication.models.enums.Gender;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Builder
 public class UpdateProfileRequest {
+
+    private LocalDate dateOfBirth;
+    private int age;
+    private Gender gender;
+    private String motherMaidenName;
+    //contact information
+    private String address;
+    private String stateOfResidence;
+    private String lgaOfResidence;
+    private String stateOfOrigin;
+    //employment information
+    private String occupation;
+    private String employerName;
+    private String employerContact;
+
+    private String bvn;
 }
