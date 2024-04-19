210b33b238bf4e275c8e803db0140ad8155f3c80
Solex202 <onwukalotachukwu210@gmail.com>
2024-04-19 23:15:34 +0100

    updating user profile

--- src/main/java/com/lota/SafeVaultBankingApplication/mapper/ApplicationMapper.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/mapper/ApplicationMapper.java
@@ -1,6 +1,8 @@
-package com.lota.SafeVaultBankingApplication.models;
+package com.lota.SafeVaultBankingApplication.mapper;
 
 import com.lota.SafeVaultBankingApplication.dtos.request.UpdateProfileRequest;
+import com.lota.SafeVaultBankingApplication.models.SafeVaultUserProfile;
+import org.mapstruct.Condition;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
@@ -8,5 +10,10 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 @Mapper(componentModel = "spring",
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface ApplicationMapper {
+
+    @Condition
+    default boolean isNotEmpty(String value) {
+        return value != null && !value.isEmpty();
+    }
     void updateProfileToSafeVaultUserProfile(UpdateProfileRequest request, @MappingTarget SafeVaultUserProfile safeVaultUserProfile);
 }
