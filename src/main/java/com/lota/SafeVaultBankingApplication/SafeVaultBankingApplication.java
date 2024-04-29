89491fa3324c4dcd5a367bd1c5046117638ea729
Solex202 <onwukalotachukwu210@gmail.com>
2024-04-29 15:12:40 +0100

    updating user account --- generating account number

--- src/main/java/com/lota/SafeVaultBankingApplication/SafeVaultBankingApplication.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/SafeVaultBankingApplication.java
@@ -3,11 +3,16 @@ package com.lota.SafeVaultBankingApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class SafeVaultBankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SafeVaultBankingApplication.class, args);
 	}
-
 }
