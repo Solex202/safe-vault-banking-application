c30a52f2fdb162e553076a5d2b1d0a95552cfd52
Solex202 <onwukalotachukwu210@gmail.com>
2024-05-01 22:36:26 +0100

    implementing transaction services

---
+++ src/main/java/com/lota/SafeVaultBankingApplication/models/Account.java
@@ -0,0 +1,29 @@
+package com.lota.SafeVaultBankingApplication.models;
+
+import com.lota.SafeVaultBankingApplication.models.enums.AccountType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+
+    @Id
+    private String accountId;
+    @DocumentReference
+    private SafeVaultUser safeVaultUser;
+    private AccountType accountType;
+    private List<String> accountNumber;
+
+    private double balance;
+    private LocalDateTime dateCreated;
+}
