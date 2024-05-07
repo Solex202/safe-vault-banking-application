443750bca2d1c5802c2a19c07436eadbff9dbb13
Solex202 <onwukalotachukwu210@gmail.com>
2024-05-07 09:48:39 +0100

    implementing fund transfer

--- src/main/java/com/lota/SafeVaultBankingApplication/repositories/AccountRepository.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/repositories/AccountRepository.java
@@ -3,6 +3,11 @@ package com.lota.SafeVaultBankingApplication.repositories;
 import com.lota.SafeVaultBankingApplication.models.Account;
 import org.springframework.data.mongodb.core.MongoAdminOperations;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface AccountRepository extends MongoRepository<Account, String> {
+    Account findBySafeVaultUserId(String userId);
+
+    @Query("{'accountNumber' : ?0}")
+    Account findByAccountNumber(String destinationAccountNumber);
 }
