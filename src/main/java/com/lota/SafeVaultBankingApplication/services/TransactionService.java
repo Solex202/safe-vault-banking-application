74fa06f0833eb6b61abd637d7552cc71bfb38afb
Solex202 <onwukalotachukwu210@gmail.com>
2024-05-07 09:50:00 +0100

    implementing fund transfer

--- src/main/java/com/lota/SafeVaultBankingApplication/services/TransactionService.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/services/TransactionService.java
@@ -1,4 +1,8 @@
 package com.lota.SafeVaultBankingApplication.services;
 
+import com.lota.SafeVaultBankingApplication.dtos.request.FundTransferDto;
+
 public interface TransactionService {
+
+    String performTransfer(String userId,FundTransferDto fundTransferDto);
 }
