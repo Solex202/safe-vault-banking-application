2b139789dac3f8b87828ec9fdd0306660bfbaa14
Solex202 <onwukalotachukwu210@gmail.com>
2024-05-08 11:51:28 +0100

    testing fund transfer

--- src/test/java/com/lota/SafeVaultBankingApplication/AccountServiceImplTest.java
+++ src/test/java/com/lota/SafeVaultBankingApplication/AccountServiceImplTest.java
@@ -1,10 +1,17 @@
 package com.lota.SafeVaultBankingApplication;
 
 
+import com.lota.SafeVaultBankingApplication.dtos.request.UpdateAccountRequest;
+import com.lota.SafeVaultBankingApplication.models.Account;
 import com.lota.SafeVaultBankingApplication.services.AccountService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class AccountServiceImplTest {
@@ -17,5 +24,16 @@ public class AccountServiceImplTest {
     void setAccountNumber(){
 
         accountService.generateUserAccountNumber("6637f08d9f73f13847de9da9");
+    }
+
+    @Test
+    void updateAccountDetails(){
+        UpdateAccountRequest request = new UpdateAccountRequest();
+        request.setDailyLimit(20000);
+        request.setTransferLimit(700000);
+
+        Account account = accountService.updateAccount("6637f08d9f73f13847de9da9", request);
+
+        assertThat(account.getDailyLimit(), is(20000));
     }
 }
