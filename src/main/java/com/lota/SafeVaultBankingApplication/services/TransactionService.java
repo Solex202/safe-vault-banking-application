a7d715fe98d4734dc20f506dfab0fa0c794e4ba0
Solex202 <onwukalotachukwu210@gmail.com>
2024-05-11 20:14:15 +0100

    view transactions implementation

--- src/main/java/com/lota/SafeVaultBankingApplication/services/TransactionService.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/services/TransactionService.java
@@ -3,9 +3,13 @@ package com.lota.SafeVaultBankingApplication.services;
 import com.lota.SafeVaultBankingApplication.dtos.request.FundTransferDto;
 import com.lota.SafeVaultBankingApplication.dtos.response.ViewTransactionResponseDto;
 
+import java.util.List;
+
 public interface TransactionService {
 
     String performTransfer(String userId,FundTransferDto fundTransferDto);
+
+    List<ViewTransactionResponseDto> viewAllTransactions(int page, int size);
 
     ViewTransactionResponseDto viewTransaction(String transactionId);
 }
