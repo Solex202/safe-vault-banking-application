a480d2079c01958ef66221ecd5049bf34334b1d6
Solex202 <onwukalotachukwu210@gmail.com>
2024-05-13 14:59:01 +0100

    exposing rest endpoints

--- src/main/java/com/lota/SafeVaultBankingApplication/controllers/SafeVaultUserController.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/controllers/SafeVaultUserController.java
@@ -1,4 +1,27 @@
 package com.lota.SafeVaultBankingApplication.controllers;
 
+import com.lota.SafeVaultBankingApplication.services.SafeVaultUserService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/users")
+@RestController
+@AllArgsConstructor
 public class SafeVaultUserController {
+
+    private final SafeVaultUserService safeVaultUserService;
+
+    @PostMapping("/process-phoneNumber")
+    public ResponseEntity<?> processPhoneNumber(@RequestBody String phoneNumber){
+
+        safeVaultUserService.processUserPhoneNumber(phoneNumber);
+
+        return new ResponseEntity<>(HttpStatus.CONTINUE);
+    }
 }
