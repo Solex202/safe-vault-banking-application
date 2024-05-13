b8c3e5fed988fa4ef501e71b9d64e96d525e3606
Solex202 <onwukalotachukwu210@gmail.com>
2024-05-13 09:47:19 +0100

    handling exceptions globally

--- src/main/java/com/lota/SafeVaultBankingApplication/exceptions/GlobalExceptionMessage.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/exceptions/GlobalExceptionMessage.java
@@ -1,4 +1,22 @@
 package com.lota.SafeVaultBankingApplication.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 public class GlobalExceptionMessage {
+
+    @ExceptionHandler()
+    public ResponseEntity<Object> handleAppException(AppException e){
+
+        ApiException apiException =
+                new ApiException(e.getMessage(),
+                        HttpStatus.BAD_REQUEST,
+                        ZonedDateTime.now(ZoneId.of("Z")));
+
+        return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
+    }
 }
