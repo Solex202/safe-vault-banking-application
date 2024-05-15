620fa9564cc955cbcefa9f2b15f89062e26430eb
Solex202 <onwukalotachukwu210@gmail.com>
2024-05-15 12:50:11 +0100

    exposing rest endpoints

--- src/main/java/com/lota/SafeVaultBankingApplication/controllers/SafeVaultUserProfileController.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/controllers/SafeVaultUserProfileController.java
@@ -2,11 +2,14 @@ package com.lota.SafeVaultBankingApplication.controllers;
 
 
 import com.lota.SafeVaultBankingApplication.dtos.request.CreateProfileRequest;
+import com.lota.SafeVaultBankingApplication.dtos.request.UpdateProfileRequest;
 import com.lota.SafeVaultBankingApplication.dtos.response.CreateProfileResponse;
+import com.lota.SafeVaultBankingApplication.models.SafeVaultUserProfile;
 import com.lota.SafeVaultBankingApplication.services.SafeVaultUserProfileService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,8 +20,15 @@ public class SafeVaultUserProfileController {
     private final SafeVaultUserProfileService safeVaultUserProfileService;
 
     @PostMapping("/create-profile")
-    public ResponseEntity<?> createProfile(@RequestParam String userId, @RequestBody CreateProfileRequest request){
+    public ResponseEntity<?> createProfile(@AuthenticationPrincipal String userId, @RequestBody CreateProfileRequest request){
         CreateProfileResponse response = safeVaultUserProfileService.createUserProfile(userId, request);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/update-profile")
+    public ResponseEntity<?> updateProfile(@AuthenticationPrincipal String userId, @RequestBody UpdateProfileRequest request){
+        SafeVaultUserProfile response = safeVaultUserProfileService.updateUserProfile(userId, request);
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
