9fb1ef47c03116aae3c106a399362c3f6a78d455
Solex202 <onwukalotachukwu210@gmail.com>
2024-05-16 10:11:40 +0100

    exposing rest endpoints

--- src/main/java/com/lota/SafeVaultBankingApplication/controllers/TransactionUserController.java
+++ src/main/java/com/lota/SafeVaultBankingApplication/controllers/TransactionUserController.java
@@ -2,15 +2,15 @@ package com.lota.SafeVaultBankingApplication.controllers;
 
 
 import com.lota.SafeVaultBankingApplication.dtos.request.FundTransferDto;
+import com.lota.SafeVaultBankingApplication.dtos.response.ViewTransactionResponseDto;
 import com.lota.SafeVaultBankingApplication.services.TransactionService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/transactions")
@@ -25,5 +25,17 @@ public class TransactionUserController {
         String response =  transactionService.performTransfer(userId, transferDto);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/get-transaction/{transactionId}")
+    public ResponseEntity<?> viewTransaction(@PathVariable String transactionId){
+        ViewTransactionResponseDto responseDto = transactionService.viewTransaction(transactionId);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/view-transactions}")
+    public ResponseEntity<?> viewAllTransactions(@RequestParam int page, @RequestParam int size){
+        List<ViewTransactionResponseDto> responseDto = transactionService.viewAllTransactions(page, size);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 }
